b06696537aa6eaf06d5648cb285d09b7f015fc76
Galen Chang <galenc12@gmail.com>
2022-09-15 15:50:28 -1000

    d

---
+++ temperatureConverter.js
@@ -0,0 +1,14 @@
+function temperatureConverter(temperature, temperatureType) {
+    if (temperatureType === "C") {
+        return (temperature * 9/5) + 32;
+    } else if (temperatureType === "F") {
+        return (temperature - 32) * 5/9;
+    } else {
+        return "llegal temperature type"
+    }
+}
+
+
+console.log(temperatureConverter(212, "F"));     // 100
+console.log(temperatureConverter(0, "C"));       // 32
+console.log(temperatureConverter(0, "X"));       // Illegal temperature type
